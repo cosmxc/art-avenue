da783cf35345f49ba863dcc70a1995f01812d107
cosmxc <liapandaraoan@gmail.com>
2023-03-27 11:31:58 +0800

    removed config override

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const ModuleScopePlugin = require('discord-webhook-node/node_modules/react-dev-utils/ModuleScopePlugin');
-
-module.exports = function override(config, env) {
-    config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
-
-    return config;
-};
